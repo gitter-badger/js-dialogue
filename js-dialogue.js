f039319b3cd2b23de998a30ec86652809512035d
Nikitaw99 <manahovman@gmail.com>
2016-01-31 17:00:14 +0300

    The main file.
    This file is the main file use to preview. It is currently bbeing worked on.

---
+++ js-dialogue.js
@@ -0,0 +1,6 @@
+document.write("Bot: Hello, dear user.<br>");
+document.write("Bot: What is your name?<br>");
+var name = prompt("What is your name?")
+document.write(name);
+document.write(": My name is ");
+document.write(name);
