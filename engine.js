6b7e9c73b3a8c4c15f6c5d636208703997b5b254
Nikita Manahov <manahovman@gmail.com>
2016-01-31 22:09:27 +0300

    Update No.2 - The Engine.
    Fixes fixes fixes!

--- engine.js
+++ engine.js
@@ -1,37 +1,37 @@
 // Welcoming message, change to anything you want.
-document.write("Bot: Hello, dear user.<br>");
+console.log("Bot: Hello, dear user.");
 // The bot asks your name, no need to change it.
-document.write("Bot: What is your name?<br>");
+console.log("Bot: What is your name?");
 var name = prompt("What is your name?")
 // You say that your name == var name;
-document.write(name + ": My name is " + name + ".<br>");
+console.log(name + ": My name is " + name + ".");
 // The dialogue continiues, and this is the first branching choice.
-document.write("Bot: How is your day going?");
+console.log("Bot: How is your day going?");
 /* The text in brackets shows what are the possible choices.
 The '...' means that any other choice will work.  */
 var text = prompt("How is your day going? (good, bad, normal, ...)")
 // When you type 'good'.
-if (text == "good") [
-  document.write(name + ": My day? It is going good.");
+if (text == "good") {
+  console.log(name + ": My day? It is going good.");
   // The bot's response.
-  document.write("Bot: Very nice! I hope they happiness continiues!");
+  console.log("Bot: Very nice! I hope they happiness continiues!");
   // The player's auto-response.
-  document.write(name + ": Ah, thank you! I hope you have a nice day too!");
-  ]
+  console.log(name + ": Ah, thank you! I hope you have a nice day too!");
+}
   // When you type 'bad'.
-  else if (text == "bad") [
-    document.write(name + ": My day? It is going badly.");
-    document.write("Bot: Don't be sad! The day is going to be good - believe it!");
-    document.write(name + ": Thank you for your support...");
-    ]
+  else if (text == "bad") {[}
+    console.log(name + ": My day? It is going badly.");
+    console.log("Bot: Don't be sad! The day is going to be good - believe it!");
+    console.log(name + ": Thank you for your support...");
+    }
     // When you type 'normal'.
     else if (text == "normal") [
-      document.write(name + ": My day? Eh, it is going normal.");
-      document.write("Bot: Cheer up! :)");
-      document.write(name + ": :)"); 
-      ]
+      console.log(name + ": My day? Eh, it is going normal.");
+      console.log("Bot: Cheer up! :)");
+      console.log(name + ": :)"); 
+      }
       // When you type something else.
-      else [
-        document.write(name + ": ...");
-        document.write("Bot: ...");
-        ]
+      else {
+        console.log(name + ": ...");
+        console.log("Bot: ...");
+      }
