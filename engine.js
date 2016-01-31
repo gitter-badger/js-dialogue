740e19ae4d253923d8ccfcafa51bf82dc1e4f61f
Nikitaw99 <manahovman@gmail.com>
2016-01-31 19:09:39 +0300

    Update No.2 - The Engine.
    Added a bit more comments!

--- engine.js
+++ engine.js
@@ -13,16 +13,25 @@ var text = prompt("How is your day going? (good, bad, normal, ...)")
 // When you type 'good'.
 if (text == "good") [
   document.write(name + ": My day? It is going good.");
+  // The bot's response.
+  document.write("Bot: Very nice! I hope they happiness continiues!");
+  // The player's auto-response.
+  document.write(name + ": Ah, thank you! I hope you have a nice day too!");
   ]
   // When you type 'bad'.
   else if (text == "bad") [
     document.write(name + ": My day? It is going badly.");
+    document.write("Bot: Don't be sad! The day is going to be good - believe it!");
+    document.write(name + ": Thank you for your support...");
     ]
     // When you type 'normal'.
     else if (text == "normal") [
       document.write(name + ": My day? Eh, it is going normal.");
+      document.write("Bot: Cheer up! :)");
+      document.write(name + ": :)"); 
       ]
       // When you type something else.
       else [
         document.write(name + ": ...");
+        document.write("Bot: ...");
         ]
