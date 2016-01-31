49eabe9835fd5cf3e99a4ef067dfa0327d3e7cef
Nikitaw99 <manahovman@gmail.com>
2016-01-31 19:05:49 +0300

    Update No.3 - The Main file.
    The bot now responds to you!

--- js-dialogue.js
+++ js-dialogue.js
@@ -6,13 +6,20 @@ document.write("Bot: How is your day going?");
 var text = prompt("How is your day going? (good, bad, normal, ...)")
 if (text == "good") [
   document.write(name + ": My day? It is going good.");
+  document.write("Bot: Very nice! I hope they happiness continiues!");
+  document.write(name + ": Ah, thank you! I hope you have a nice day too!");
   ]
   else if (text == "bad") [
     document.write(name + ": My day? It is going badly.");
+    document.write("Bot: Don't be sad! The day is going to be good - believe it!");
+    document.write(name + ": Thank you for your support...");
     ]
     else if (text == "normal") [
       document.write(name + ": My day? Eh, it is going normal.");
+      document.write("Bot: Cheer up! :)");
+      document.write(name + ": :)");
       ]
       else [
         document.write(name + ": ...");
+        document.write("Bot: ...");
         ]
