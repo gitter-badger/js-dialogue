5453ce8f7b1640f710625e0230218e4e80259b7c
Nikitaw99 <manahovman@gmail.com>
2016-01-31 18:28:58 +0300

    Update No.2 - The Main File.
    Yey! The dialogue has more outcomes now!

--- js-dialogue.js
+++ js-dialogue.js
@@ -1,4 +1,18 @@
 document.write("Bot: Hello, dear user.<br>");
 document.write("Bot: What is your name?<br>");
 var name = prompt("What is your name?")
-document.write(name + ": My name is " + name + ".");
+document.write(name + ": My name is " + name + ".<br>");
+document.write("Bot: How is your day going?");
+var text = prompt("How is your day going? (good, bad, normal, ...)")
+if (text == "good") [
+  document.write(name + ": My day? It is going good.");
+  ]
+  else if (text == "bad") [
+    document.write(name + ": My day? It is going badly.");
+    ]
+    else if (text == "normal") [
+      document.write(name + ": My day? Eh, it is going normal.");
+      ]
+      else [
+        document.write(name + ": ...");
+        ]
